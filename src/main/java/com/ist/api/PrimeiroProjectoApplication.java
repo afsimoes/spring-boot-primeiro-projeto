d468b96dafd65aa7a852f485c1e3c9f2e5e0f8b9
Álvaro Simões <alvarez_simm_7@hotmail.com>
2018-04-18 18:43:14 +0100

    first commit

---
+++ src/main/java/com/ist/api/PrimeiroProjectoApplication.java
@@ -0,0 +1,12 @@
+package com.ist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjectoApplication.class, args);
+	}
+}
